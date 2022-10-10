40fd0656ba05056f6bf31e474823871f89029d67
Stephen Gruzin <stephengrzn@gmail.com>
2022-10-10 16:15:07 -0700

    First commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,69 @@
+<script type="ts">
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	const selectDoc = (e: any) => {
+		goto(e.target.value);
+	};
+</script>
+
+<header>
+	<a href="/"><h1>Developer</h1></a>
+	<div class="paths">
+		<a href="/" class="{$page.url.pathname === '/' ? 'true' : ''} route">Home</a>
+		<a href="/about" class="{$page.url.pathname === '/about' ? 'true' : ''} route">About</a>
+		{#if $page.url.pathname.includes('/documentation')}
+			<div class="docul">
+				<a href="/documentation" class="true route">Docs</a>
+				<select on:change={selectDoc}>
+					<option value="/documentation">Select Doc</option>
+					<option value="/documentation/gruzauth">GruzAuth</option>
+				</select>
+			</div>
+		{:else}
+			<a href="/documentation" class="route">Docs</a>
+		{/if}
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 10px;
+		height: 70px;
+		width: 100vw;
+		background-color: #f3f3f3;
+	}
+
+	.paths {
+		display: flex;
+		gap: 10px;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.route {
+		font-family: "Roboto", sans-serif;
+		color: black;
+		padding: 10px;
+		border-radius: 5px;
+	}
+
+	h1 {
+		color: black;
+		font-family: 'Poppins', sans-serif;
+		font-weight: bold;
+	}
+	
+	select {
+		border: 1px solid black;
+		padding: 10px 5px;
+		border-radius: 5px;
+	}
+
+	.true {
+		background-color: #dfdfdf;
+	}
+</style>
---
+++ src/routes/documentation/gruzauth/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>GruzAuth | Documentation</title>
+	<meta name="description" content="The GruzAuth documentation for developers." />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Docs for gruzauth</h1>
+</div>
+
+<style>
+	h1 {
+		font-family: "Poppins", sans-serif;
+	}
+</style>
